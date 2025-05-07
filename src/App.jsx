3ada3a712328ccce010f75e13312c6f66c47d3ba
Kyle-gar <kylegar45@gmail.com>
2025-05-07 00:12:16 -0500

    Update App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PredictionDashboard from "./components/PredictionDashboard";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center">
+      <PredictionDashboard />
+    </div>
+  );
+}
+
+export default App;
